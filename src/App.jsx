330d41271bb61c6e063454c155307d7e8bab3935
shabana <pshabana2712@gmail.com>
2025-02-08 16:03:33 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes,Navigate } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Header from "./Header/Header";
 import HraAssesment from "./Pages/HRA/Hra/HraAssesment";
 import Summarydetails from "./Pages/HRA/PersonalAndFamily/SummarryDetails";
@@ -8,29 +8,31 @@ import Login from "./Login/Login";
 import Dashboard from "./Pages/PhrDashboard/Dashboard";
 import { useLocation } from "react-router-dom";
 import Phr from "./Pages/PHR/Phr";
-import OverviewPhrUpdate from './Pages/PHR/PhrNavbar/Overview/Allergies/AllergiesUpdate/OverviewPhrUpdate'
+import OverviewPhrUpdate from "./Pages/PHR/PhrNavbar/Overview/Allergies/AllergiesUpdate/OverviewPhrUpdate";
 
 
-function App()   {
-  const location = useLocation()
-  console.log(location)
-  const HideHeaderandFooter = location.pathname === '/Login'
-  console.log(HideHeaderandFooter)
-  return ( 
-  <>
+
+
+function App() {
+  const location = useLocation();
+  console.log(location);
+  const HideHeaderandFooter = location.pathname === "/Login";
+  console.log(HideHeaderandFooter);
+  return (
+    <>
       {!HideHeaderandFooter && <Header />}
       <Routes>
-        <Route path='/' element={<Navigate to='/Login' />} />
-        <Route path="/phr" element={<Phr/>} />
-        <Route path='/Login' element={<Login/>}/>
-        <Route path="/Dashboard" element={<Dashboard/>} />
+        <Route path="/" element={<Navigate to="/Login" />} />
+        <Route path="/phr" element={<Phr />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
         <Route path="/hraassessment/new" element={<HraAssesment />} />
         <Route path="/hraassessment/edit" element={<HraAssesment />} />
-        <Route path="/HraDashboard/:id" element= {<HraMainDashboard/>} />
-        <Route path='/assessment' element={<Summarydetails />} />
-        <Route path='/OverviewPhrUpdate' element={<OverviewPhrUpdate/>} />
+        <Route path="/HraDashboard/:id" element={<HraMainDashboard />} />
+        <Route path="/assessment" element={<Summarydetails />} />
+        <Route path="/OverviewPhrUpdate" element={<OverviewPhrUpdate />} />
       </Routes>
-   {!HideHeaderandFooter &&  <Footer/>}
+      {!HideHeaderandFooter && <Footer />}
     </>
   );
 }
