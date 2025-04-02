f8d6ca68aa0d46f7c6a1f86a2b991eaf08e16ce4
aravindgutlapalli <130845992+aravindgutlapalli@users.noreply.github.com>
2025-04-02 20:27:56 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,6 @@ import DateProvider from "./utils/DateProvider.jsx";
 import store from "./store/store.jsx";
 import {Provider } from "react-redux";
 
-
-
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
   <BrowserRouter>
