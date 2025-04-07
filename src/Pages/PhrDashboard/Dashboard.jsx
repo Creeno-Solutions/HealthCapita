62fd0d3fad05cf046e558142e451d3f53d85dd9c
shabana <pshabana2712@gmail.com>
2025-04-07 19:37:14 +0530

    updated dashboard

--- src/Pages/PhrDashboard/Dashboard.jsx
+++ src/Pages/PhrDashboard/Dashboard.jsx
@@ -4,10 +4,14 @@ import QuickToolsAndCard from "./QuickToolsAndCard";
 const Dashboard = () => {
   return (
     <div>
-    <div className=' px-14 py-4 bg-[#F9FAFB]'>
-     <h1 className='text-3xl'>Welcome Back John! </h1>
-    </div>
-    <QuickToolsAndCard/>
+      <div className="flex justify-between items-center px-14 py-4 bg-[#F9FAFB]">
+        <h1 className="text-3xl">Welcome Back John! </h1>
+        <h1 className="text-base text-[#374151]">
+          Last Login :{" "}
+          <span className="text-base font-semibold">4 April, 2025</span>
+        </h1>
+      </div>
+      <QuickToolsAndCard />
     </div>
   );
 };
